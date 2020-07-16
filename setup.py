fed64f11992212eb149f3c21ef50b3e470306d80
Poissonfish <chun-peng.chen@wsu.edu>
2020-07-16 02:42:54 -0700

    update docs

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 import setuptools
 setuptools.setup(name='photo_grid',
-                 version='1.1.93',
+                 version='1.1.94',
                  description='A GUI for field segmentation',
                  url='https://github.com/Poissonfish/GRID',
                  python_requires='>=3.6',
