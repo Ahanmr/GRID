4cebbcf58870f16fa5896ebeadb43be8585f9729
Poissonfish <chun-peng.chen@wsu.edu>
2020-07-17 10:27:55 -0700

    update docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -66,8 +66,8 @@ html_static_path = ['_static']
 html_logo = 'res/GRID_banner.png'
 
 
-# def setup(app):
-#     app.add_javascript("google_analytics.js")
-#     app.add_javascript(
-#         "https://www.googletagmanager.com/gtag/js?id=UA-151005724-3")
+def setup(app):
+    app.add_javascript("google_analytics.js")
+    app.add_javascript(
+        "https://www.googletagmanager.com/gtag/js?id=UA-151005724-3")
 
